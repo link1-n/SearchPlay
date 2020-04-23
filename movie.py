3f811f3552252b4d4c0cfdb7243874d367570b26
sp1nalcord <55630723+sp1nalcord@users.noreply.github.com>
2020-04-23 18:02:27 +0530

    Added run again and changed ask options.

--- movie.py
+++ movie.py
@@ -5,12 +5,19 @@ init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
 from termcolor import cprint
 from pyfiglet import figlet_format
 
-cprint(figlet_format('SearchPlay', font='standard'),
-       'cyan', attrs=['bold'])
-ask = str(input('\n\nEnter 1 if you want to stream.\nEnter 2 if you torrent.\n'))
-if ask == '2':
-    call(["python", "torrent.py"])
-    print('\n\nScript made by u/link1-n and u/sp1nalcord.')
-else:
-    call(["python", "watch.py"])
-    print('\n\nScript made by u/link1-n and u/sp1nalcord.')
+cprint(figlet_format('SearchPlay', font='standard'), 'cyan', attrs=['bold', 'underline', 'dark','concealed'])
+def run():
+    ask = str(input("\nEnter 'Stream' if you want to stream.\nEnter 'Torrent' if you torrent.\n"))
+    if ask == 'Torrent':
+        call(["python", "torrent.py"])
+        print('\n\nScript made by u/link1-n and u/sp1nalcord.')
+    else:
+        call(["python", "watch.py"])
+        print('\n\nScript made by u/link1-n and u/sp1nalcord.')
+
+    ask_again = str(input('Do you want to run again?'))
+    if ask_again=='yes' or 'YES' or 'Yes':
+        run()
+    else:
+        print('')
+run()
